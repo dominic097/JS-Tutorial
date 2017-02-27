a5593812d13f7145ad82154e1f28ff3a4250fdd1
Dominic <dominic097@gmail.com>
2017-02-27 15:56:52 +0530

    added Day-05 and 06

--- Day-04/generators.js
+++ Day-04/generators.js
@@ -12,21 +12,53 @@ console.log(gen.next().value); // 2
 
 
 //-----------------------------------------------------------
-function* fibonacci() {
-  let [prev, curr] = [0, 1];
-  yield prev;
-  yield curr;
-  while (true) {
-    [prev, curr] = [curr, prev + curr];
-    yield curr;
-  }
-}
- 
-for (const n of fibonacci()) {
-  if (n > 100) break;
-  console.log(n);
+let fibonacci = {
+    [Symbol.iterator]() {
+        let pre = 0, cur = 1
+        return {
+           next () {
+               [ pre, cur ] = [ cur, pre + cur ]
+               return { done: false, value: cur }
+           }
+        }
+    }
 }
 
+for (let n of fibonacci) {
+    if (n > 1000)
+        break
+    console.log(n)
+}
+
+
+// ---------------------------------------------------------------
+
+
+// ES5
+
+var fibonacci = {
+    next: (function () {
+        var pre = 0, cur = 1;
+        return function () {
+            tmp = pre;
+            pre = cur;
+            cur += tmp;
+            return cur;
+        };
+    })()
+};
+
+var n;
+for (;;) {
+    n = fibonacci.next();
+    if (n > 1000)
+        break;
+    console.log(n);
+}
+
+// ---------------------------------------------------------------------
+
+// using Generators
 
 let fib = {
   * [Symbol.iterator]() {
---
+++ Day-06-Node/express/ex_GET.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Dominic on 2/27/2017.
+ */
+
+var express = require('express');
+var app=express();
+
+app.get('/get_example3', function (req, res) {
+    res.send('<p>Firstname: ' + req.query['firstname']+'</p> <p>Lastname: '+req.query['lastname']+'</p><p>Password: '+req.query['password']+'</p> <p>AboutYou: '+req.query['aboutyou']+'</p>');
+})
+
+var server = app.listen(8000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
